e2fe370c0e6ea96807f116e919896d28b0240557
Konstantin <splinebd@mail.ru>
2020-04-17 00:22:20 +0500

    Adding files from my local machine

--- dist/index-my.ae07151c.js
+++ dist/index-my.ae07151c.js
@@ -213,7 +213,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "49517" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "54301" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
