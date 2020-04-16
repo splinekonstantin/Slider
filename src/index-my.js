a53529823515721b176b3f00ce78d3dfc3c509b4
Konstantin <splinebd@mail.ru>
2020-04-17 00:09:24 +0500

    Adding files from my local machine

---
+++ src/index-my.js
@@ -0,0 +1,61 @@
+function Slider(slider) {
+  if (!(slider instanceof Element)) {
+    throw new Error('No slider passed in');
+  }
+  //   create some variables for working with the slider
+  let current;
+  let prev;
+  let next;
+  //   select the elements needed for the slider
+  const slides = slider.querySelector('.slides');
+  const prevButton = slider.querySelector('.goToPrev');
+  const nextButton = slider.querySelector('.goToNext');
+
+  function startSlider() {
+    current = slider.querySelector('.current') || slides.firstElementChild;
+    prev = current.previousElementSibling || slides.lastElementChild;
+    next = current.nextElementSibling || slides.firstElementChild;
+    console.log(next);
+  }
+
+  function applyClasses() {
+    current.classList.add('current');
+    prev.classList.add('prev');
+    next.classList.add('next');
+  }
+
+  function move(direction) {
+    //   first strip all the classes from the current slides
+    const classesToRemove = ['prev', 'current', 'next'];
+    prev.classList.remove(...classesToRemove);
+    current.classList.remove(...classesToRemove);
+    next.classList.remove(...classesToRemove);
+    if (direction === 'back') {
+      //    make an array of the new values, and destructure them over and into the prev, current and next variables
+      [prev, current, next] = [
+        prev.previousElementSibling || slides.lastElementChild,
+        prev,
+        current,
+      ];
+    } else {
+      [prev, current, next] = [
+        current,
+        next,
+        next.nextElementSibling || slides.firstElementChild,
+      ];
+    }
+
+    applyClasses();
+  }
+
+  //   when this slider is created, run the stsrtSlider function
+  startSlider();
+  applyClasses();
+
+  //   Event Listeners
+  prevButton.addEventListener('click', () => move('back'));
+  nextButton.addEventListener('click', move);
+}
+
+const mySlider = Slider(document.querySelector('.slider'));
+const dogSlider = Slider(document.querySelector('.dog-slider'));
